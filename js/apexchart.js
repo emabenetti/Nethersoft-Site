386a56451db966f2893de76f2493a975929a508a
Emanuel Benetti <emanuelbenetti39@gmail.com>
2021-03-22 15:32:11 -0300

    Agregadas mas cosas a alarms.html

--- js/apexchart.js
+++ js/apexchart.js
@@ -77,7 +77,62 @@ var options2 = {
     labels: ["Progress"]
 };
 
+var options3 = {
+    chart: {
+        height: 280,
+        type: "radialBar",
+    },
+
+    series: [30],
+    colors: ["#20E647"],
+    plotOptions: {
+        radialBar: {
+            hollow: {
+                margin: 0,
+                size: "70%",
+                background: "#293450"
+            },
+            track: {
+                dropShadow: {
+                    enabled: true,
+                    top: 2,
+                    left: 0,
+                    blur: 4,
+                    opacity: 0.15
+                }
+            },
+            dataLabels: {
+                name: {
+                    offsetY: -10,
+                    color: "#fff",
+                    fontSize: "13px"
+                },
+                value: {
+                    color: "#fff",
+                    fontSize: "30px",
+                    show: true
+                }
+            }
+        }
+    },
+    fill: {
+        type: "gradient",
+        gradient: {
+            shade: "dark",
+            type: "vertical",
+            gradientToColors: ["#87D4F9"],
+            stops: [0, 100]
+        }
+    },
+    stroke: {
+        lineCap: "round"
+    },
+    labels: ["Alarmas disponibles"]
+};
+
 
 new ApexCharts(document.querySelector("#chart1"), options1).render();
 
 new ApexCharts(document.querySelector("#chart2"), options2).render();
+
+new ApexCharts(document.querySelector("#chart3"), options3).render();
